e925f2eef58d2c7af9768a90fd7f1cd1021f4fc5
meedaah19 <hameedattimileyinoyewopo@gmail.com>
2026-04-21 19:42:18 +0100

    feat: enable CORS support in the application

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import {router}  from './routes/user.js'
 import {expenseRouter} from './routes/expenses.js';
@@ -11,6 +12,7 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use(router);
